5e7cdc86b10123f1dfe767ca265a1af1a3df5753
Thomas <nyumat18@gmail.com>
2024-02-23 23:19:47 -0800

    🔧 config: added beaver svg + site config

--- src/config/nav-menu.ts
+++ src/config/nav-menu.ts
@@ -3,71 +3,28 @@ import type { NavMenuConfig } from "@/types";
 export const navMenuConfig: NavMenuConfig = {
   infosNav: [
     {
-      title: "Menu",
+      title: "Directory",
       items: [
         {
-          title: "Features",
-          href: "/#features",
-          description: "Take a closer look at the project's features.",
+          title: "What We Do",
+          href: "/#what-we-do",
+          description: "What our club is all about.",
         },
         {
-          title: "Twitter (@miickasmt)",
-          href: "https://twitter.com/miickasmt",
-          description: "Follow me to get the latest updates and news.",
-          external: true,
+          title: "Past Projects",
+          href: "/#past-projects",
+          description:
+            "Projects are the heart of our club. See what we've done.",
         },
         {
-          title: "Source Code",
-          href: "https://github.com/mickasmt/astro-nomy",
-          description: "You want to star the repository ? Let's get started!",
-          external: true,
+          title: "Our Team",
+          href: "/about/#team",
+          description: "Meet the leadership team and learn about our mission.",
         },
       ],
     },
   ],
-  examplesNav: [
-    {
-      title: "Examples",
-      items: [
-        {
-          title: "Blog",
-          href: "/blog",
-          description: "A Markdown/MDX blog built using Content Collections.",
-        },
-        {
-          title: "Docs",
-          href: "/docs/getting-started",
-          description:
-            "A Markdown/MDX documentation site built using Content Collections.",
-        },
-        {
-          title: "Authentification",
-          href: "/login",
-          description: "Login and register pages for authentification.",
-          disabled: true,
-        },
-        {
-          title: "Dashboard",
-          href: "/#dashboard",
-          description: "A dashboard panel after authentification.",
-          disabled: true,
-        },
-        {
-          title: "Ecommerce",
-          href: "/#examples/ecommerce",
-          description:
-            "Different pages of an ecommerce app fetching data from an API.",
-          disabled: true,
-        },
-        {
-          title: "Social Media",
-          href: "/#examples/social-media",
-          description: "Different components & pages of an social media app.",
-          disabled: true,
-        },
-      ],
-    },
-  ],
+
   links: [
     {
       title: "FAQ",
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,16 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "OSUAPP",
+  displayName: "App Club at OSU",
+  name: "App Development Club at Oregon State University",
   description:
     "The App Development Club is a student organization at Oregon State University that focuses on teaching students how to develop applications for mobile and web platforms.",
   url: "https://astro-nomy.vercel.app",
   ogImage: "https://astro-nomy.vercel.app/og.jpg",
   links: {
-    twitter: "https://twitter.com/miickasmt",
+    ideal_logic: "https://apps.ideal-logic.com/osusli",
     github: "https://github.com/OSU-App-Club",
+    discord: "https://discord.gg/fJpwFbKDMz",
+    old_website: "https://osuapp.club",
   },
 };

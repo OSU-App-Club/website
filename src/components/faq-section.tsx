1a66d0b1dd5fdb13925e466634db927fcac9c979
Thomas <nyumat18@gmail.com>
2024-02-24 01:05:40 -0800

    🚀 routes: FAQ page w/ accordions

--- src/components/faq-section.tsx
+++ src/components/faq-section.tsx
@@ -8,33 +8,33 @@ import {
 const faqData = [
   {
     id: "item-1",
-    question: "What does the club do?",
+    question: "What exactly does the club do?",
     answer:
-      "Our club organizes various events and activities for its members, including workshops, social gatherings, and practical projects to learn and practice App Development.",
+      "The App Development Club provides resources, funding, and mentorship to students interested in App Development. We host workshops, hackathons, and networking events throughout the year to help our members learn and grow their skillset.",
   },
   {
     id: "item-5",
     question: "Do I need to have experience in App Development to join?",
     answer:
-      "No, you do not need to have any experience in App Development to join the club. We welcome students of all skill levels, from beginners to experts.",
+      "No, you do not need to have any experience in App Development to join the club. We welcome students of all skill levels, from beginners learning to code to seasoned experts in App Development. Our club is a great place to find out what App Development is all about.",
   },
   {
     id: "item-2",
     question: "When are the club meetings?",
     answer:
-      "We meet once a week on Wednesdays at 6:00 PM. The location of the meeting is both on Zoom, and in person at the Learning Innovation Center (LINC) Room 303 on campus.",
+      "We meet once a week on Wednesdays at 6:00 PM. The location of the meeting is both on Zoom, and in person at OSU's very own Learning Innovation Center (LINC) Room 303.",
   },
   {
     id: "item-3",
-    question: "How do I join the club?",
+    question: "How do I join the App Development Club?",
     answer:
-      "You can join the club by signing up on our Ideallogic. Membership is free and open to all students. After signing up, come join us at our next meeting!",
+      "All you need to do is come to our meetings! We welcome all students to join us at our meetings, and we encourage you to bring a friend. To help us get more funding, we ask that you sign up for our club on IdealLogic, and join our Discord server. We're the most active on Discord, and it's the best way to stay up to date with our events.",
   },
   {
     id: "item-4",
     question: "When was the club founded?",
     answer:
-      "The club was founded in 2015 by a small group of students who wanted to create a community for students interested in App Development. Since then, the club has grown to over 500+ members.",
+      "The club was founded in 2015 by a small group (8) of students who wanted to form a community for students interested in mobile software development. Since then, the club has grown to over 500+ members, and has expanded to include web development, and other software development technologies.",
   },
 ];
 

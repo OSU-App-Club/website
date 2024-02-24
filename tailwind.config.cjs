74445fbae828bc985e67f7ec9fcdb9ddfd243470
Thomas <nyumat18@gmail.com>
2024-02-23 23:18:05 -0800

    🎨 extend typography + spotlight keyframe

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,12 +16,13 @@ module.exports = {
       fontFamily: {
         sans: ["Inter", ...fontFamily.sans],
         heading: ["CalSans Semibold", ...fontFamily.sans],
+        subheading: ["CalSans Regular", ...fontFamily.sans],
       },
       height: {
-        "18": "4.5rem",
+        18: "4.5rem",
       },
       spacing: {
-        "18": "4.5rem",
+        18: "4.5rem",
       },
       colors: {
         border: "hsl(var(--border))",
@@ -72,10 +73,21 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
     },
   },

7aa6506fd60d900e520463fe9af80fb887fb467a
FireIsGood <fireisadequate@gmail.com>
2024-04-30 18:45:33 -0700

    🐛 fix: RSS endpoint
    The export had to be capitalized

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { siteConfig } from "@/config/site";
 
-export async function get(context) {
+export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
     title: siteConfig.name,
